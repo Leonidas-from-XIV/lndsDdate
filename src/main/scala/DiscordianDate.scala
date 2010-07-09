4cc173b16cfceed308abecbb2dda4c7fbedcb190
Marek Kubica <marek@xivilization.net>
2010-07-09 12:52:54 +0200

    Finished Discordian date port

--- src/main/scala/DiscordianDate.scala
+++ src/main/scala/DiscordianDate.scala
@@ -1,24 +1,54 @@
+/*
+ * Discordian date in Scala
+ *
+ * Algorithm from
+ * <http://imrannazar.com/Discordian-Date-Handling-in-Java>
+ *
+ */
+
 import java.util.Date;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 class DiscordianDate(date: Date) {
-  var seasonNames = {
+  val seasonNames = {
     "Chaos"; "Discord"; "Confusion"; "Bureucracy"; "The Aftermath"};
-  var dayNames = {
+  val dayNames = {
     "Sweetmorn"; "Boomtime"; "Pungenday"; "Prickle-Prickle"; "Setting Orange"};
 
 
-  var baseCalendar: GregorianCalendar = new GregorianCalendar();
+  val baseCalendar: GregorianCalendar = new GregorianCalendar();
   baseCalendar.setTime(date);
-  var year = baseCalendar.get(Calendar.YEAR) + 1166;
-  var yearDay = baseCalendar.get(Calendar.DAY_OF_YEAR);
-  var isLeap = baseCalendar.isLeapYear(baseCalendar.get(Calendar.YEAR));
-  var yd = yearDay - 1;
-  if (isLeap && yd > 59)
-    yd = yd - 1;
+  val year = baseCalendar.get(Calendar.YEAR) + 1166;
+  val yearDay = baseCalendar.get(Calendar.DAY_OF_YEAR);
+  val isLeap = baseCalendar.isLeapYear(baseCalendar.get(Calendar.YEAR));
 
-  var season = (yd / 73) + 1;
-  var weekDay = (yd % 5) + 1;
-  var seasonDay = (yd % 73) + 1;
+  val yd = if (isLeap && yearDay > 60) yearDay - 2 else yearDay - 1;
+
+  val season = (yd / 73) + 1;
+  val weekDay = (yd % 5) + 1;
+  val seasonDay = (yd % 73) + 1;
+
+  val seasonName = seasonNames(season-1);
+  val dayName = dayNames(yearDay-1);
+
+  override def toString(): String = {
+    if (isLeap && yearDay == 59) {
+      return "St. Tib's Day, " + year.toString();
+    }
+    else {
+      return dayNames(weekDay-1) + ", " +
+        seasonNames(season-1) + " " +
+        seasonDay.toString() + ", " +
+        year.toString()
+    }
+  }
+
+  def getTime(): Date = {
+    var sentinelCalendar = new GregorianCalendar();
+    sentinelCalendar.set(Calendar.YEAR, year - 1166);
+    sentinelCalendar.set(Calendar.DAY_OF_YEAR, yearDay);
+
+    return sentinelCalendar.getTime();
+  }
 }
