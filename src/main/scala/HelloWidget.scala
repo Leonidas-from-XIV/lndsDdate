3d04330da9bd9c90daf2cd326f93051abdfb8107
Marek Kubica <marek@xivilization.net>
2010-07-10 11:18:14 +0200

    Made the widget refresh

--- src/main/scala/HelloWidget.scala
+++ src/main/scala/HelloWidget.scala
@@ -1,6 +1,17 @@
-package net.xivilization.ddate
+package net.xivilization.ddate;
 
-import _root_.android.appwidget.AppWidgetProvider
+import _root_.android.appwidget.AppWidgetProvider;
+import _root_.android.appwidget.AppWidgetManager;
+import _root_.android.content.Context;
+import _root_.android.widget.RemoteViews;
 
 class HelloWidget extends AppWidgetProvider {
+  override def onUpdate(context: Context, appWidgetManager: AppWidgetManager,
+    appWidgetIds: Array[Int]) = {
+    val updateViews = new RemoteViews(context.getPackageName(), R.layout.main);
+    updateViews.setTextViewText(R.id.widget_textview, "Foo");
+    appWidgetManager.updateAppWidget(appWidgetIds, updateViews);
+
+    super.onUpdate(context, appWidgetManager, appWidgetIds);
+  }
 }
